b44e5f3c6ecdfe2c12e924ea10ca2bbd4c3f224f
Jason Harriot <jasonharriot@gmail.com>
2021-04-11 16:00:59 -0400

    misc

--- calculations.m
+++ calculations.m
@@ -1,2 +1,18 @@
 %%ENES220 Crane Project
 %
+
+cableSF = 2;
+liveLoadMax = 100000;
+cableUT = 200000;
+motorHP = 50; %hp
+
+pulleyRatio = 4;
+
+cableDiameter = sqrt(cableSF*liveLoadMax *4 / (pulleyRatio * cableUT * pi))
+
+minDrumOD = 20*cableDiameter
+
+cableTension = liveLoadMax / pulleyRatio
+
+motorRPM = motorHP * 63024 / (minDrumOD*.5*cableTension)
+
