821ccd6cab8bea4b11e6fee44138d3106333a480
Jason Harriot <jasonharriot@gmail.com>
2021-04-11 16:54:49 -0400

    Added cable speed, lift time

--- calculations.m
+++ calculations.m
@@ -7,6 +7,7 @@ cableUT = 200000;
 motorHP = 50; %hp
 
 pulleyRatio = 4;
+liftHeight = 30*12 %inches
 
 cableDiameter = sqrt(cableSF*liveLoadMax *4 / (pulleyRatio * cableUT * pi))
 
@@ -14,7 +15,12 @@ minDrumOD = 20*cableDiameter
 
 cableTension = liveLoadMax / pulleyRatio
 
+%HP = lb*in*RPM / 63024
 motorRPM = motorHP * 63024 / (minDrumOD*.5*cableTension*cableSF)
+
+cableSpeed = minDrumOD*pi*motorRPM/60 %in/sec
+
+liftTime = liftHeight/cableSpeed %seconds
 
 J = (pi/32) * minDrumOD^4
 TorsionalShearStress = (cableTension* cableSF * (minDrumOD/2))/J
